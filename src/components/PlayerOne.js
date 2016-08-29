2e5409349aaf6eaca096f2b9c5d51a663532024b
Hiro Narita <hiro.narita@gmail.com>
2016-08-29 10:24:36 -0400

    new files

---
+++ src/components/PlayerOne.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PlayerOne extends Component {
+  constructor() {
+    super();
+    this.state = {
+      margin: 0,
+      arrayPosition: 0
+    }
+  }
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default PlayerOne;
